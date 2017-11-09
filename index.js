0d9cb86904df5b2c534ff8d6219a4a1eb0a806ad
Laura M. Travin <laura.travin@gmail.com>
2017-11-09 21:55:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ function takeANumber(katzDeliLine, name){
    var position =  katzDeliLine.length;
      katzDeliLine.push(name);  //I add new person to line
      return `Welcome, ${name}. You are number ${position + 1} in line.`;
-
 }
