c314b993cdab912787acfbbd5e333f4dc6af77e3
Laura M. Travin <laura.travin@gmail.com>
2017-11-09 22:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ var katzDeliLine = [];
 function takeANumber(katzDeliLine, name){
    var position =  katzDeliLine.length;
      katzDeliLine.push(name);  //I add new person to line
+     if (position< 7){
      return `Welcome, ${name}. You are number ${position + 1} in line.`;
+    }
 }
 
 function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
   var served = katzDeliLine[0];
   katzDeliLine.shifts();
-  return  `Currently serving .`;
-  
-  
+  return  `Currently serving ${serverd}.`;
+  } else {
+     return 'The line is currently empty.';
+
+  }
 }
